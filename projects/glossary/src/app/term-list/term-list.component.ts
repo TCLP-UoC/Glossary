51ee6a0e4182ba17f6410e3f2cf41747728e8abd
Adam Kariv <adam.kariv@gmail.com>
2022-07-11 13:39:41 +0300

    Add by-letter filtering

--- projects/glossary/src/app/term-list/term-list.component.ts
+++ projects/glossary/src/app/term-list/term-list.component.ts
@@ -11,11 +11,20 @@ export class TermListComponent implements OnInit {
   private _terms: any[] = [];
   terms: any[] = [];
   queryStr: string = '';
+  activeLetter_: string = '';
+  letters: string[] = [];
 
   constructor(public airtable: AirtableService) {
     airtable.terms.subscribe(terms => {
       this._terms = terms;
       this.terms = terms;
+      for (const term of terms) {
+        const title: string = term.title.toUpperCase();
+        if (!this.letters.includes(title[0])) {
+          this.letters.push(term.title[0]);
+        }
+        this.letters.sort()
+      }
     });
   }
 
@@ -24,10 +33,33 @@ export class TermListComponent implements OnInit {
 
   set query(value: string) {
     if (value) {
+      this.activeLetter_ = '';
       this.terms = this._terms.filter(term => term.title.toLowerCase().includes(value.toLowerCase()));
     } else {
       this.terms = this._terms;
     }
     this.queryStr = value;
   }
+
+  get query(): string {
+    return this.queryStr;
+  }
+
+  set activeLetter(value: string) {
+    if (this.activeLetter === value) {
+      value = '';
+    }
+    this.activeLetter_ = value;
+    this.query = '';
+    if (value) {
+      this.terms = this._terms.filter(term => term.title.toLowerCase().startsWith(value.toLowerCase()));
+    } else {
+      this.terms = this._terms;
+    }
+  }
+
+  get activeLetter(): string {
+    return this.activeLetter_;
+  }
+
 }
